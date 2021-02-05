b77ab68c58610de63fd25df8fabb9482830cfde6
Christopher Kim <chrisnkim1999@gmail.com>
2021-02-04 19:09:46 -0500

    added questions

--- index.js
+++ index.js
@@ -3,7 +3,55 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project'
+    },
+    {
+        type: 'input',
+        name: 'tablecontent',
+        message: 'Table of Contents'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'License'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contributing'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Any questions'
+    },
+];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
